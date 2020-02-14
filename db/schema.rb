889fa1aa28f5bb1db15d80d8253a53c842d23b9c
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2020-02-14 13:40:03 +0000

    userテーブルに生年月日カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191108074435) do
+ActiveRecord::Schema.define(version: 20200214133649) do
 
   create_table "users", force: :cascade do |t|
     t.string "guardian"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20191108074435) do
     t.datetime "leave_time"
     t.string "fix_day"
     t.datetime "fix_time"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
