27448d4efa957fee03d404926d5b35656f855a89
EC2 Default User <ec2-user@ip-172-31-43-247.us-east-2.compute.internal>
2020-02-22 14:31:12 +0000

    6_2エラーメッセージの日本語化対応完了_add

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,6 +1,7 @@
 class NoticesController < ApplicationController
   def index
     @notices = Notice.all
+    @notices = Notice.paginate(page: params[:page], per_page: 5)
   end
   
   def new
--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -1,2 +1,18 @@
 module NoticesHelper
+    
+  # 一時的セッションにいるユーザーを返す。
+  # 現在ログインしているユーザーがいる場合オブジェを返す。
+  
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
 end
