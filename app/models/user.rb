24d6624d0e98433f2dd3591f38a070b2f5e8f7a7
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2020-02-27 10:17:14 +0000

    10_1生徒登録実装, ブランチ名変更_add

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,14 @@ class User < ApplicationRecord
   
   before_save { self.email = email.downcase }
 
-  validates :guardian,  presence: true, length: { maximum: 50 }
-  validates :student,  presence: true, length: { maximum: 50 }
+  validates :guardian, length: { maximum: 50 }
+  validates :student, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 100 },
+  validates :email, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true    
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  validates :school, presence: true
-  # validates :school_year, presence: true
-  validates :birthday, presence: true
-  validates :fix_day, presence: true
-  validates :fix_time, presence: true
-  validates :birthday, presence: true
   
   validate :class_hoice
   
