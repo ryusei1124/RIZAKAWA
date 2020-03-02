bba7a3ceefc6643060ec6fed79d91fbfff37d8e4
EC2 Default User <ec2-user@ip-172-31-41-234.ap-northeast-1.compute.internal>
2020-03-02 14:22:42 +0000

    ユーザーコントローラー修正、各モデルの関連付け

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,5 @@
 class Lesson < ApplicationRecord
+  belongs_to :user
   attr_accessor :regularkanji
   attr_accessor :starttime
   attr_accessor :finishtime
--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,5 +1,6 @@
 class Notice < ApplicationRecord
-    default_scope -> { order(updated_at: :desc) }
-    validates :notice_title,  presence: true
-    validates :notice_content, presence: true
+  belongs_to :user
+  default_scope -> { order(updated_at: :desc) }
+  validates :notice_title,  presence: true
+  validates :notice_content, presence: true
 end
