e9b14e4813ebcff8e55435a01172bcce1bd755dd
T.Ito <your.email@example.com>
2020-03-14 04:23:33 +0000

    20-4ユーザー側の予約詳細画面_add

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,34 @@
 class ReservationsController < ApplicationController
+  include ApplicationHelper
+  before_action:unless_login
+  
   def index
   end
   
   def reservations_log
   end
+  
+  def useredit
+    student_id=params[:student_id]
+    @student=Student.find(student_id)
+    lesson_id=params[:lesson_id]
+    @lesson=Lesson.find(lesson_id)
+    @reservation=Reservation.find_by(lesson_id:@lesson.id, student_id:@student.id)
+    @zoomnumber=Reservation.where(" lesson_id=? AND zoom  = ?" ,@lesson.id, true).count
+    @realnumber=Reservation.where(" lesson_id=? AND zoom  = ?" ,@lesson.id, false).count
+  end
+  def userupdate
+    reservation_id=params[:reservation_id]
+    zoom=params[:zoom]
+    @beforesite=params[:beforesite]
+    reservation=Reservation.find(reservation_id)
+    reservation.zoom=tob(zoom)
+    if reservation.save
+      flash[:success]="受講方法を変更しました"
+    else
+      flash[:danger]="受講方法に失敗しました。"
+    end
+    redirect_to request.referrer
+  end
+  
 end
