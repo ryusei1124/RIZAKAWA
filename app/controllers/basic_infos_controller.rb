365a38e2b2fdaf0573e5b29695a7886035adde78
Toshinori-Ito <johndoe@example.com>
2020-04-13 12:09:56 +0900

    40_4予約情報不具合修正_edit

--- app/controllers/basic_infos_controller.rb
+++ app/controllers/basic_infos_controller.rb
@@ -1,16 +1,16 @@
 class BasicInfosController < ApplicationController
   def student_update
-    studentsmany_params.each{|id,val|birthday,school = val.values_at("birthday","school")
-    @student = Student.find(id)
-    @student.update(
-        school:school,
-        birthday:birthday)
-    }
-    flash[:success]="基本情報更新に成功しました"
+    students = studentsmany_params
+    students.each do | id, school |
+      sc = students[id.to_s]["school"]
+      @student = Student.find(id)
+      @student.update(school:sc)
+    end
+    flash[:success] = "基本情報更新に成功しました"
     redirect_to request.referrer
   end
   private
   def studentsmany_params
-    params.permit(studentsmany: [:birthday,:school])[:studentsmany]
+    params.permit(studentsmany: [:school])[:studentsmany]
   end
 end
