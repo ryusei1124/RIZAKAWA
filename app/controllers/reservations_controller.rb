49ddd69cb1db7d51a097d355a6e8c2b632bbf601
EC2 Default User <ec2-user@ip-172-31-28-110.us-east-2.compute.internal>
2020-04-25 09:29:37 +0000

    出席ボタン系機能実装完了

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,52 +3,60 @@ class ReservationsController < ApplicationController
   before_action:unless_login
   require 'date'
   
-  UPDATE_ERROR_MSG = "出席時間の登録に失敗しました。"
-  
   def index
   end
   
   def update
     @reservation = Reservation.find(params[:id])
     
-    if @reservation.attendance_time.nil?
-      if @reservation.update_attributes(attendance_time: Time.current.change(sec: 0))
-        flash[:success] = "出席時間を登録しました。"
-      else
-        flash[:danger] = UPDATE_ERROR_MSG
-      end
-    elsif @reservation.leave_time.nil?
-      if @reservation.update_attributes(leave_time: Time.current.change(see: 0))
-        flash[:success] = "退席時間を登録しました。"
-      else
-        flash[:danger] = UPDATE_ERROR_MSG
-      end
-      redirect_to request.referrer
+    if params[:no] == "1"
+        if @reservation.update_attributes(attendance_time: Time.current.change(sec: 0))
+          flash[:success] = "出席時間を登録しました。"
+        else
+          flash[:danger] = "出席時間の登録に失敗しました。"
+        end
+       redirect_to request.referrer and return
     end
-  end
+      
+    if params[:no] == "2"
+        if @reservation.update_attributes(leave_time: Time.current.change(sec: 0))
+          flash[:success] = "退席時間を登録しました。"
+        else
+          flash[:danger] = "退席時間の登録に失敗しました。"
+        end
+       redirect_to request.referrer and return
+    end
   
-  def aaaa
-    if @reservation.absence.nil?
-      
-      if @reservation.attributes = { absence: true }
-        flash[:success] = "該当受講生の欠席登録しました。"
-      else
-        flash[:danger]  = "該当受講生の欠席登録に失敗しました。"
-      end
+    if params[:no] == "3"
+        if @reservation.update_attributes(attendance_time: nil, leave_time: nil)
+          flash[:success] = "出席と退席時間を消去しました。"
+        else
+          flash[:danger] = "出席と退席時間の消去に失敗しました。"
+        end
+       redirect_to request.referrer and return
     end
-     redirect_to request.referrer
-  end
-      
-  def destroy
-    @reservation = Reservation.find(params[:id])
+
+    if params[:no] == "4"
+        if @reservation.update_attributes(absence: true)
+          flash[:success] = "該当受講生の欠席登録しました。"
+        else
+          flash[:danger]  = "該当受講生の欠席登録に失敗しました。"
+        end
+       redirect_to request.referrer and return
+    end
     
-    @reservation.destroy
-    flash[:success] = "出席時間と退席時間を削除しました。"
-    
-    redirect_to request.referrer
+    if params[:no] == "5"
+        if @reservation.update_attributes(absence: false)
+          flash[:success] = "欠席登録を解除しました。"
+        else
+          flash[:danger] = "欠席登録の解除に失敗しました。"
+        end
+       redirect_to request.referrer and return
+    end
   end
   
   def reservations_log
     @user = User.find_by(params[:id])
   end
+  
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,14 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
-<<<<<<< HEAD
-  
-  validate :leave_time_is_invalid_without_a_attendance_time
-  
-  def leave_time_is_invalid_without_a_attendance_time
-    errors.add(:attendance_time, "が必要です。") if attendance_time.blank? && leave_time.present?
-  end
-=======
   belongs_to :student
->>>>>>> 60781f4b5edc0fd918c447eda3d57b087d9301fa
+  
 end
