1cf7752e5941af135c69de7cdc40d56122d6f5b1
EC2 Default User <ec2-user@ip-172-31-28-110.us-east-2.compute.internal>
2020-05-16 09:50:19 +0000

    58_1生徒新規登録アコーディオン削除_edit

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,7 @@ class LessonsController < ApplicationController
   before_action :schoolgrade
   before_action :set_student
   before_action :set_lesson
+  before_action :admin_user, only: %i(lesson_detail)
   
   require 'date'
   include ApplicationHelper
