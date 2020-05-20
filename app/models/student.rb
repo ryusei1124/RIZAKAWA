c0a2e439d96e6226d31d2a75e7839968acfcd039
Toshinori-Ito <johndoe@example.com>
2020-05-20 20:13:47 +0900

    62_1ユーザー一覧フォント修正、管理者側予約詳細フォント・セル幅修正_edit

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
    belongs_to :user
+   has_many :questions, dependent: :destroy
    validates :birthday,  presence: true
    validates :student_name,  presence: true
    has_many :reservations, dependent: :destroy
