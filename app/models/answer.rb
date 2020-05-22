de0159b9253b9e4600fd0be561e3b2b4bd112270
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2020-05-22 09:28:36 +0000

    63_5問い合わせ_edit

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
-  # belongs_to :student
   default_scope -> { order(created_at: :desc) }
   
   validates :answer_content, presence: true
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :user
-  # belongs_to :student
   has_many :answers, dependent: :destroy
   scope :created_at_order, -> { order(created_at: :desc) }
   
