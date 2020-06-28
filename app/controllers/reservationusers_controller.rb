263284681353e2c7da485d0c90a30db519830731
T.Ito <your.email@example.com>
2020-06-28 17:08:56 +0900

    78_1生徒固定時間一覧表モーダル追加_edit

--- app/controllers/reservationusers_controller.rb
+++ app/controllers/reservationusers_controller.rb
@@ -55,7 +55,7 @@ class ReservationusersController < ApplicationController
     end
     @reservation.fix_time = nil
     @reservation.transfer = true
-    @reservation.cancel = true
+    @reservation.cancel = false
     if @reservation.note.present?
       @reservation.note = @reservation.note + " " + l(@lesson_meeting_on.to_datetime, format: :day_week) + "分の振替です"
     else
