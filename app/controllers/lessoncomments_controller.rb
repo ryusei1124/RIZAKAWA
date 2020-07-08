65f2ffa712557ffbdfec3fa578d5aed47b37e90d
T.Ito <your.email@example.com>
2020-07-08 11:29:00 +0900

    81_1受講生基本情報修正_edit-2

--- app/controllers/lessoncomments_controller.rb
+++ app/controllers/lessoncomments_controller.rb
@@ -20,7 +20,7 @@ class LessoncommentsController < ApplicationController
       link = "reservationusers/useredit?reservation_id=#{lessoncomment.reservation_id}&student_id=#{lessoncomment.student_id}"
       @link = @url + link
       bcc = ""
-      UserMailer.send_mail( @destination_user, @send_user, bcc, title, content, @link).deliver_now
+      #UserMailer.send_mail( @destination_user, @send_user, bcc, title, content, @link).deliver_now
 
       flash[:success] = "コメント登録に成功し、お知らせメールを送信しました"
     else
