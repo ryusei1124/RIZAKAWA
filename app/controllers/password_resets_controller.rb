06ec62d9e6ca6215365126307e7c0e8af5b7cce1
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2020-07-08 14:24:39 +0000

    test

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -50,6 +50,7 @@ class PasswordResetsController < ApplicationController
       unless (@user && @user.activated? &&
               @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
+        flash[:danger] = "ユーザーが違う"
       end
     end
     
