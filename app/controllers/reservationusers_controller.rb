e95de8524eb5c5c10964026e52055432d1ac6582
T.Ito <your.email@example.com>
2020-07-11 06:03:34 +0900

    82_4予約履歴見た目修正等_edit-2

--- app/controllers/reservationusers_controller.rb
+++ app/controllers/reservationusers_controller.rb
@@ -208,6 +208,6 @@ class ReservationusersController < ApplicationController
       @send_user =  current_user
       link = "reservationusers/useredit?reservation_id=#{@reservation.id}&student_id=#{@reservation.student_id}"
       @link = @url + link
-      ＃UserMailer.send_mail( @destination_user, @send_user, @bcc, @title, @content,@link).deliver_now
+      #UserMailer.send_mail( @destination_user, @send_user, @bcc, @title, @content,@link).deliver_now
     end
 end
