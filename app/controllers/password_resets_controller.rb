3f9f850c2570d2faccc50b195008fb954c0d3095
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2020-07-12 03:44:00 +0000

    パスワード再設定修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,7 +29,7 @@ class PasswordResetsController < ApplicationController
     elsif @user.update_attributes(user_params)          
       log_in @user
       flash[:success] = "パスワードがリセットされました。"
-      redirect_to　root_url
+      redirect_to root_url
     else
       render 'edit'                                     
     end
