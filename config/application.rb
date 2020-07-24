440752e5c95e4b76167e16403cd7e62ee528c3c0
T.Ito <your.email@example.com>
2020-07-24 11:40:19 +0900

    86_1固定終了時間追加_edit

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module RizakawaApp
     config.i18n.default_locale = :ja
     config.active_record.default_timezone = :local 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    config.action_view.automatically_disable_submit_tag = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
