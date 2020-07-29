4cd41f0cdb0d3d9d0589ca46ba01a59a77d83a09
T~Ito <example@example.com>
2020-07-29 21:24:14 +0900

    87_1ユーザー確認用基本情報に固定時間終了の表示追加_edit-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,36 +36,9 @@ class UsersController < ApplicationController
         zoom = " Zoom" if st.zoom?
         student_name = st.student_name + " (" + Student.gradeyear( st.id ) + examinee + zoom + ")" 
         @student_ls.push([ids,st.fix_day,time, student_name,finishtime])
-        if st.fix_day2.present?  
-          num = @weekday.index(st.fix_day2).to_i
-          time = timedisplay(st.fix_time2)
-          finishtime =  timedisplay(st.fix_finishtime2)
-          ids = ((num+1)*10000 + (st.fix_time2.hour.to_i)*100 + st.fix_time2.min.to_i).to_i
-          @student_ls.push([ids,st.fix_day2,time, student_name,finishtime])
-        end
-        if st.fix_day3.present?  
-          num = @weekday.index(st.fix_day3).to_i
-          time =  timedisplay(st.fix_time3)
-          finishtime =  timedisplay(st.fix_finishtime3)
-          ids = ((num+1)*10000 + (st.fix_time3.hour.to_i)*100 + st.fix_time3.min.to_i).to_i
-          @student_ls.push([ids,st.fix_day3,time, student_name,finishtime])
-        end
-        if st.fix_day4.present?  
-          num = @weekday.index(st.fix_day4).to_i
-          time =  timedisplay(st.fix_time4)
-          finishtime =  timedisplay(st.fix_finishtime4)
-          ids = ((num+1)*10000 + (st.fix_time4.hour.to_i)*100 + st.fix_time4.min.to_i).to_i
-          @student_ls.push([ids,st.fix_day4,time, student_name,finishtime])
-        end
-        if st.fix_day5.present?  
-          num = @weekday.index(st.fix_day5).to_i
-          time =  timedisplay(st.fix_time5)
-          finishtime =  timedisplay(st.fix_finishtime5)
-          ids = ((num+1)*10000 + (st.fix_time5.hour.to_i)*100 + st.fix_time5.min.to_i).to_i
-          @student_ls.push([ids,st.fix_day5,time, student_name,finishtime])
-        end
+        
       end
-      #@student_ls.sort!
+      @student_ls.sort!
   end
   
   def new
