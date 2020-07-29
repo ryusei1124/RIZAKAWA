60de01b32c238c609235f70b800c23dc3f39b385
T~Ito <example@example.com>
2020-07-29 18:13:42 +0900

    87_1ユーザー確認用基本情報に固定時間終了の表示追加_edit-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   include ApplicationHelper
   
   def index
-    @admins = User.user_kanaorder
+    #@admins = User.user_kanaorder
     #@admins = User.where(admin:true)
     @weekday = ["月","火","水","木","金","土","日"]
     if params[:cation] == "2"
